da9a38548107ab34137b30790084a926ac5f25b6
vvoev <vladovoev@yahoo.co.uk>
2017-01-17 19:29:31 +0200

    exam has been finished with 100/100

--- 06.JavaScript/PreparationForExam/1.Valleys.js
+++ 06.JavaScript/PreparationForExam/1.Valleys.js
@@ -24,8 +24,7 @@ function solve(input) {
     console.log(maxValleySum)
 
 
-//function PEAK
-    //demo function
+
     function isPeak(index) {
         if (index === 0 || index === numbers.length - 1) {
             return true;
