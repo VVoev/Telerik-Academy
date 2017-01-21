38b358604f932d9960a3fd2a067512fde518446e
vvoev <vladovoev@yahoo.co.uk>
2017-01-21 17:50:47 +0200

    Course Unit Testing Has been Started

---
+++ 07.Unit Testing with C#/UnitTesting/UnitTesting/StartUp.cs
@@ -0,0 +1,16 @@
+﻿namespace UnitTesting
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    class StartUp
+    {
+        static void Main()
+        {
+
+        }
+    }
+}
