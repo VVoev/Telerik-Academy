917ae42a3df4ec4c6ba192ad2367d6b8646e5b57
vvoev <vladovoev@yahoo.co.uk>
2017-01-22 14:25:04 +0200

    Added Nunit Demo

---
+++ 07.Unit Testing with C#/01. Unit Testing/Nunit/Bank/Account.cs
@@ -0,0 +1,47 @@
+﻿namespace Bank
+{
+    using System;
+    public class Account
+    {
+        private float balance;
+
+        public void Deposit(float amount)
+        {
+            if (amount == 0)
+            {
+                throw new ArgumentException("Cannot deposit amount of 0.00");
+            }
+            balance += amount;
+        }
+
+        public void WithDraw(float amount)
+        {
+            if (amount == 0)
+            {
+                throw new ArgumentException("Cannot withdrawn amount of 0.00");
+            }
+            balance -= amount;
+        }
+
+        public void TransferFunds(Account targetAcc,float amount)
+        {
+            if (targetAcc == this)
+            {
+                throw new ArgumentException("You cannot transfer money to yourself");
+            }
+            balance = amount;
+            targetAcc.balance += amount;
+        }
+
+        public float Balance
+        {
+            get
+            {
+                return this.balance;
+            }
+        }
+
+
+   
+    }
+}
---
+++ 07.Unit Testing with C#/01. Unit Testing/Nunit/Bank/Bank.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Bank
+{
+    public class Bank
+    {
+        private List<Account> accounts;
+
+        public Bank()
+        {
+            accounts = new List<Account>();
+        }
+
+        public int AccountsCount
+        {
+            get
+            {
+                return accounts.Count;
+            }
+        }
+
+        public Account this [int index]
+        {
+            get
+            {
+                if(index<0 || index> accounts.Count)
+                {
+                    throw new ArgumentException("Invalid account index");
+                }
+                Account account = (Account)accounts[index];
+                return account;
+            }
+        }
+
+        public void AddAccount(Account acc)
+        {
+            if (acc == null)
+            {
+                throw new ArgumentException("Account cannot be null");
+            }
+            accounts.Add(acc);
+        }
+
+        public void RemoveAcc(Account acc)
+        {
+            int index = accounts.IndexOf(acc);
+            if (index == -1)
+            {
+                throw new ArgumentException("Account not found , Can not be removed");
+            }
+            accounts.Remove(acc);
+        }
+    }
+}
