a81255b7e75639259af321ef310035d77b7cb6ba
vvoev <vladovoev@yahoo.co.uk>
2017-01-24 20:15:31 +0200

    added Recurssion examples

---
+++ 03.C#Advanced/08.Recurssion/01.Catalan Number/StartUp.cs
@@ -0,0 +1,29 @@
+﻿namespace _01.Catalan_Number
+{
+    using System;
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+            var arr = new int[] { 5, 10, 15,20,25 };
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(RecussionCatalan(arr[i]));
+            }
+          
+        }
+
+        private static int RecussionCatalan(int n)
+        {
+            if (n == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return n * RecussionCatalan (n - 1);
+            }
+        }
+    }
+}
---
+++ 03.C#Advanced/08.Recurssion/02.Fibonachi/NestedLoops.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.NestedLoops
+{
+    class NestedLoops
+    {
+        private static int[] arr;
+        private static int k;
+        private static int n;
+        private static int counter;
+
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine($"Enter a number N");
+            n = int.Parse(Console.ReadLine());
+            k = n;
+            arr = new int[k];
+            Generate(0);
+            Console.WriteLine(counter);
+
+        }
+
+        private static void Generate(int index)
+        {
+            //Recurssion bottom
+            if (index >= k)
+            {
+                //Console.WriteLine(string.Join(" ",arr));
+                counter++;
+            }
+            else
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    arr[index] = i + 1;
+                    Generate(index + 1);
+                }
+
+            }
+        }
+    }
+}
