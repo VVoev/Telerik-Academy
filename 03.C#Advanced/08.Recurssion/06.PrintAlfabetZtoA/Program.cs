81f8fa9411f491ff0d152816048faa7750c9824f
vvoev <vladovoev@yahoo.co.uk>
2017-01-25 21:50:18 +0200

    added alfabet with recurssion

---
+++ 03.C#Advanced/08.Recurssion/06.PrintAlfabetZtoA/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06.PrintAlfabetZtoA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char a = '@';
+            PrintAlfabet(a);
+        }
+
+        private static char PrintAlfabet(char a)
+        {
+            if (a == 'Z')
+            {
+                return a;
+            }
+            else
+            {
+                Console.WriteLine($"ASii number{(int)a+1} Letter: "+PrintAlfabet((char)(a+1)));
+                return a;
+            }
+            
+        }
+    }
+}
