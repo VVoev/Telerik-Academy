6424ff672e7f8e4bb8f1baba5a2b8123c6179a5c
VVoev <vladovoev@yahoo.co.uk>
2017-02-03 19:45:19 +0200

    added tasker

---
+++ 07.Unit Testing with C#/03. Mocking and JustMock/Live/ProjectTasker/Tasker.Tests/Core/Providers/IdProviderTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tasker.Core.Providers;
+
+namespace Tasker.Tests.Core.Providers
+{
+    [TestFixture]
+    public class IdProviderTests
+    {
+        [Test]
+        [Category("Tasker")]
+        public void NextId_ShouldReturnIncrementedValue_WhenInvoced()
+        {
+            var idProvider = new IdProvider();
+            var resultOne = idProvider.NextId();
+            var resultTwo = idProvider.NextId();
+            Assert.IsTrue(1 == resultOne);
+            Assert.IsTrue(2 == resultTwo);
+        }
+    }
+}
---
+++ 07.Unit Testing with C#/03. Mocking and JustMock/Live/ProjectTasker/Tasker.Tests/TaskManagerTest.cs
@@ -0,0 +1,72 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tasker.Core;
+using Tasker.Core.Contracts;
+using Tasker.Core.Providers;
+using Tasker.Models.Contracts;
+using Telerik.JustMock;
+
+namespace Tasker.Tests
+{
+    [TestFixture]
+    class TaskManagerTest
+    {
+        [Test]
+        [Category("Tasker")]
+        public void Add_ShouldLogSuccesfullyMessage_WhenPassedValidTask()
+        {
+            var loggerMock = Mock.Create<ILogger>();
+            var idProviderMock = Mock.Create<IdProvider>();
+            var taskMock = Mock.Create<ITask>();
+
+            var manager = new TaskManager(idProviderMock, loggerMock);
+            manager.Add(taskMock);
+
+            Mock.Assert(() => loggerMock.Log(Arg.AnyString), Occurs.Once());
+        }
+
+        [Test]
+        [Category("Tasker")]
+        public void Remove_ShouldRemoveSuccesfully()
+        {
+            var loggerMock = Mock.Create<ILogger>();
+            var idProviderMock = Mock.Create<IdProvider>();
+            var firstTask = Mock.Create<ITask>();
+            var secondTask = Mock.Create<ITask>();
+
+
+            var manager = new TaskManager(idProviderMock, loggerMock);
+            manager.Add(firstTask);
+            manager.Add(secondTask);
+            manager.Remove((int)(firstTask.Id));
+
+
+            Assert.AreEqual(manager.Members().Count, 1);
+        }
+
+        [Test]
+        [Category("Tasker")]
+        public void AddesSuccesfullyFiveTask_WhenFiveTaskHasBeedAddedToManageR()
+        {
+            var loggerMock = Mock.Create<ILogger>();
+            var idProviderMock = Mock.Create<IdProvider>();
+            var manager = new TaskManager(idProviderMock, loggerMock);
+
+            var task = Mock.Create<ITask>();
+            var task1 = Mock.Create<ITask>();
+            var task2 = Mock.Create<ITask>();
+            var task3 = Mock.Create<ITask>();
+            var task4 = Mock.Create<ITask>();
+
+            manager.Add(task); manager.Add(task1); manager.Add(task2);manager.Add(task3);manager.Add(task4);
+
+            Assert.AreEqual(manager.Members().Count, 5);
+        }
+
+       
+    }
+}
