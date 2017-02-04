50eb7e70d7b2eeebf5fe24f033c6a637c41f6ced
VVoev <vladovoev@yahoo.co.uk>
2017-02-04 19:56:32 +0200

    Module and patter third demo

--- 08. JavaScript-OOP/03. Modules-and-Patterns/Live/3. revealing-module.js
+++ 08. JavaScript-OOP/03. Modules-and-Patterns/Live/3. revealing-module.js
@@ -1,3 +1,29 @@
-/**
- * Created by Vladimir on 2/4/2017.
- */
+var people = (function () {
+    var people = ['Poko','Loko'];
+    _render();
+    
+    function _render() {//private function
+        console.log(people);
+    }
+    
+    function addPerson(name) {
+        people.push(name);
+        _render();
+    }
+
+    function removePerson(i) {
+        people.splice(i,1);
+        _render();
+    }
+
+    return{
+        addPerson:addPerson,
+        deletePerson:removePerson
+    };
+})();
+
+people.addPerson('Jako');
+people.deletePerson(2);
+people.deletePerson(0);
+people.deletePerson(0);
+
