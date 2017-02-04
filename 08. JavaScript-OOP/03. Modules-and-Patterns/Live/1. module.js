b02593b936862c7271795f76e0b771cdcc8c0513
VVoev <vladovoev@yahoo.co.uk>
2017-02-04 17:03:06 +0200

    Module and patter first demo

--- 08. JavaScript-OOP/03. Modules-and-Patterns/Live/1. module.js
+++ 08. JavaScript-OOP/03. Modules-and-Patterns/Live/1. module.js
@@ -1,40 +1,46 @@
 var controls = (function () {
-    function formatResult(name, value) {
-        return name + ' says the result is ' + value;
+    function formatResult(name, value,action) {
+        return name + ' says the result is ' + value+' and its been performing function '+action;
     }
 
     class Calculator {
-        constructor(name) {
+        constructor(name,action) {
             this.name = name;
             this.result = 0;
+            this.action = action;
+
         };
 
         add(x) {
             x=Number(x);
             this.result += x;
+            this.action="add";
             return this;
         };
 
         subtract(x) {
             x=Number(x);
             this.result -= x;
+            this.action="minus";
             return this;
         };
 
         deduct(x) {
             x=Number(x);
             this.result /=x;
+            this.action="deduct";
             return this;
         };
 
         multiply(x) {
             x=Number(x);
             this.result *=x;
+            this.action="multiply";
             return this;
         };
 
         showResult() {
-            console.log(formatResult(this.name, this.result));
+            console.log(formatResult(this.name, this.result,this.action));
             return this;
         };
     };
@@ -43,7 +49,11 @@ var controls = (function () {
 } ());
 let calc = controls.getCalculator('First');
 calc.add(13);
+calc.showResult();
 calc.subtract(3);
+calc.showResult();
 calc.multiply(5);
+calc.showResult();
 calc.deduct(2);
 calc.showResult();
+
