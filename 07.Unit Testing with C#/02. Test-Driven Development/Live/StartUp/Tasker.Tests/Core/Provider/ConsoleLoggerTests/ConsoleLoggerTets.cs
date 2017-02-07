6ce283446ffec13be611ceb7dffcc188d23ccc44
VVoev <vladovoev@yahoo.co.uk>
2017-02-07 20:07:15 +0200

    added consoleTests

--- 07.Unit Testing with C#/02. Test-Driven Development/Live/StartUp/Tasker.Tests/Core/Provider/ConsoleLoggerTests/ConsoleLoggerTets.cs
+++ 07.Unit Testing with C#/02. Test-Driven Development/Live/StartUp/Tasker.Tests/Core/Provider/ConsoleLoggerTests/ConsoleLoggerTets.cs
@@ -1,12 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using NUnit.Framework;
+using System;
+using System.IO;
+using Tasker.Core.Providers;
 
 namespace Tasker.Tests.Core.Provider
 {
+    [TestFixture]
     class ConsoleLoggerTets
     {
+        [Category("Console")]
+        [TestCase("Pesho")]
+        [TestCase(null)]
+        [TestCase("VeryLongStringjsakhskjashaklhsaklshaklshjaklshakl")]
+        [Test]
+        public void Log_ShouldLogToConsole_WhenInvoked(string value)
+        {
+            var message = value;
+            var sut = new ConsoleLogger();
+
+            var outputStream = new StringWriter();
+            var defaultStream = Console.Out;
+            Console.SetOut(outputStream);
+
+            sut.Log(message);
+
+            //Assert
+            Assert.AreEqual(message+Environment.NewLine, outputStream.ToString());
+        }
     }
 }
---
+++ 07.Unit Testing with C#/02. Test-Driven Development/Live/StartUp/Tasker/Core/Providers/ConsoleLogger.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tasker.Core.Providers
+{
+    public class ConsoleLogger
+    {
+        public void Log(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
