780d680497c8db7c548c230a6643d640428d3627
VVoev <vladovoev@yahoo.co.uk>
2017-02-07 19:56:04 +0200

    added Provider

---
+++ 07.Unit Testing with C#/02. Test-Driven Development/Live/StartUp/Tasker.Tests/Core/Provider/IdProviderTests/IdProviderTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using Tasker.Core.Providers;
+
+namespace Tasker.Tests.Core.Provider
+{
+    [TestFixture]
+    public class IdProviderTests
+    {
+       [Category("Provider")]
+       [TestCase(0)]
+       [TestCase(1)]
+       [TestCase(2)]
+
+        [Test]
+       public void NextIdShouldIncremenentValue_WhenInvoked(int value)
+        {
+            var sut = new IdProvider();
+            var result = value;
+
+            Assert.AreEqual(result, sut.NextId());
+        }
+
+    }
+}
---
+++ 07.Unit Testing with C#/02. Test-Driven Development/Live/StartUp/Tasker/Core/Providers/IdProvider.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Tasker.Core.Providers
+{
+    public class IdProvider
+    {
+        private static int currentId = 0;
+        public int NextId()
+        {
+            return currentId++;
+        }
+    }
+}
