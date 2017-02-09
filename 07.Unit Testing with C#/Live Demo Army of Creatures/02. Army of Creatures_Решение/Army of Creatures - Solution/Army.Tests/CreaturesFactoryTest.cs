dd1ec625502c054f6b065f60bc3ad8379b1bf0c4
VVoev <vladovoev@yahoo.co.uk>
2017-02-09 11:49:11 +0200

    finish creature factory tests.

---
+++ 07.Unit Testing with C#/Live Demo Army of Creatures/02. Army of Creatures_Решение/Army of Creatures - Solution/Army.Tests/CreaturesFactoryTest.cs
@@ -0,0 +1,48 @@
+﻿using ArmyOfCreatures.Extended;
+using ArmyOfCreatures.Logic;
+using NUnit.Framework;
+using System;
+
+namespace Army.Tests
+{
+    [TestFixture]
+    public class CreaturesFactoryTest
+    {
+        [Test]
+        [Category("Creatures Factory")]
+        [TestCase("Angel")]
+        [TestCase("Archangel")]
+        [TestCase("ArchDevil")]
+        [TestCase("Behemoth")]
+        [TestCase("Devil")]
+        public void CreatureMakingSuccesfully_WhenProvidedValidCreature(string creature)
+        {
+            var factory = new ExtendedCreaturesFactory();
+            var expectedCreature = factory.CreateCreature(creature);
+
+            Assert.AreEqual(expectedCreature.GetType().Name, creature);
+        }
+
+        [Test]
+        [Category("Creatures Factory")]
+        [TestCase("Zebra")]
+        [TestCase("Rabbit")]
+        [TestCase("Cat")]
+        public void CreatureFactoryThrowingError_WhenProvidedInvalidCreature(string creature)
+        {
+            var factory = new ExtendedCreaturesFactory();
+
+            try
+            {
+                var expectedCreature = factory.CreateCreature(creature);
+            }
+            catch (ArgumentException ex)
+            {
+
+                Assert.AreEqual($"Invalid creature type \"{creature}\"!", ex.Message);
+            }
+           
+        }
+
+    }
+}
