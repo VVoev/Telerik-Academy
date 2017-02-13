5af703b98adaddc674f8e6edf34c609f423a5dde
VVoev <vladovoev@yahoo.co.uk>
2017-02-13 15:28:08 +0200

    added Test Users

--- 07.Unit Testing with C#/05. Workshop (Students)/Academy/Workshop/Academy.Tests/Models/CourseTests/EndingDateTests.cs
+++ 07.Unit Testing with C#/05. Workshop (Students)/Academy/Workshop/Academy.Tests/Models/CourseTests/EndingDateTests.cs
@@ -1,12 +1,50 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Academy.Models;
+using NUnit.Framework;
+using System;
 
 namespace Academy.Tests.Models.CourseTests
 {
-    class EndingDateTests
+    [TestFixture]
+    public class EndingDateTests
     {
+        [Category("EndingDate")]
+        [Test]
+
+        public void ReturnTheCorrectValue_WhenCalledWithGetMethod()
+        {
+            //Arrange && Act
+            var course = new Course("some course", 5, new DateTime(), new DateTime(2017, 2, 14));
+
+            //Assert
+            Assert.AreEqual(new DateTime(2017, 2, 14), course.EndingDate);
+        }
+
+        [Category("EndingDate")]
+        [Test]
+
+        public void ThrowException_WhenInvalidStartDateIsUsed()
+        {
+            //Arrange
+            var course = new Course("some course", 5, new DateTime(), new DateTime(2017, 2, 14));
+
+            //Act && Assert
+            Assert.Throws<ArgumentNullException>(() => course.EndingDate = null);
+        }
+
+        [Category("EndingDate")]
+        [Test]
+
+        public void SetDesiredNewDate_WhenNewDateIsSet()
+        {
+            //Arrange
+            var course = new Course("some course", 5, new DateTime(), new DateTime(2017, 2, 14));
+
+            //Act
+            course.EndingDate = new DateTime(2011, 5, 5);
+
+            //Assert
+            Assert.AreEqual(course.EndingDate, new DateTime(2011, 5, 5));
+
+        }
     }
 }
