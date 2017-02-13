f059406748d752954f110720e3bde184029b53bb
VVoev <vladovoev@yahoo.co.uk>
2017-02-13 13:12:25 +0200

    name test finished

---
+++ 07.Unit Testing with C#/05. Workshop (Students)/Academy/Workshop/Academy.Tests/Models/CourseTests/NameTests.cs
@@ -0,0 +1,38 @@
+﻿using Academy.Models;
+using NUnit.Framework;
+using System;
+
+namespace Academy.Tests.Models.CourseTests
+{
+    [TestFixture]
+    public class NameTests
+    {
+        [Category("Name")]
+        [Test]
+        public void ReturnTheProperWhenGetMethodIsCalled()
+        {
+            //Arrange
+            var course = new Course("some course", 5, new DateTime(), new DateTime());
+
+            //Act
+            var result = course.Name;
+
+            //Asert
+            Assert.AreEqual(result, "some course");
+            
+        }
+        [Category("Name")]
+        [Test]
+        [TestCase("I")]
+        [TestCase("very long string should throw exception very long string should throw exception very long string should throw exception very long string should throw exception very long string should throw exception very long string should")]
+        public void ThrowsArgumentExceptionWhenInvalidValuesArePassed(string name)
+        {
+            //Arrange && Act
+            var course = new Course("some name", 5, new DateTime(), new DateTime());
+
+            //Asert
+            Assert.Throws<ArgumentException>(() => course.Name = name);
+
+        }
+    }
+}
