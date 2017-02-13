9fb668910c4a0351324131962365bfdc50ce8a88
VVoev <vladovoev@yahoo.co.uk>
2017-02-13 13:20:29 +0200

    added lectures tests

---
+++ 07.Unit Testing with C#/05. Workshop (Students)/Academy/Workshop/Academy.Tests/Models/CourseTests/LecturesPerWeekTest.cs
@@ -0,0 +1,40 @@
+﻿using Academy.Models;
+using NUnit.Framework;
+using System;
+
+namespace Academy.Tests.Models.CourseTests
+{
+    [TestFixture]
+    public class LecturesPerWeekTest
+    {
+        [Category("Lectures")]
+        [Test]
+        
+        public void ReturnTheProperWhenGetMethodIsCalled()
+        {
+            //Arrange
+            var course = new Course("some course", 5, new DateTime(), new DateTime());
+            //Act
+            var result = course.LecturesPerWeek;
+
+            //Asert
+            Assert.AreEqual(result, 5);
+        }
+
+        [Category("Lectures")]
+        [Test]
+        [TestCase (0)]
+        [TestCase (8)]
+
+
+        public void ShouldThrowExceptionWhenInvalidParametersArePassed(int number)
+        {
+            //Arrange
+            var course = new Course("some course", 5, new DateTime(), new DateTime());
+            //Act
+            var result = number;
+            //Asert
+            Assert.Throws<ArgumentException>(() => course.LecturesPerWeek = number);
+        }
+    }
+}
