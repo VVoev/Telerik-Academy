a511c637b18c19215d558c157c734d5728264a30
VVoev <vladovoev@yahoo.co.uk>
2017-02-13 15:33:53 +0200

    added courses tests

---
+++ 07.Unit Testing with C#/05. Workshop (Students)/Academy/Workshop/Academy.Tests/Models/SeasonTests/ListCoursesTests.cs
@@ -0,0 +1,48 @@
+﻿using Academy.Models;
+using Academy.Models.Contracts;
+using Academy.Models.Enums;
+using Moq;
+using NUnit.Framework;
+
+namespace Academy.Tests.Models.SeasonTests
+{
+    [TestFixture]
+    public class ListCoursesTests
+    {
+        [Category("TestCourses")]
+        [Test]
+        public void IterateOverTheCollection_WhenTheCollectionIsNotEmpty()
+        {
+            // Arrange
+            var season = new Season(2016, 2017, Initiative.SoftwareAcademy);
+
+            var courseMock = new Mock<ICourse>();
+            courseMock.Setup(x => x.ToString()).Returns("");
+
+            season.Courses.Add(courseMock.Object);
+
+            //Act
+            var test = season.ListCourses();
+
+            //Assert
+            courseMock.Verify(x => x.ToString(), Times.Once);
+
+        }
+        [Category("TestCourses")]
+        [Test]
+        public void IterateOverTheCollection_WhenTheCollectionIsEmpty()
+        {
+            //Arrange
+            var season = new Season(2016, 2017, Initiative.SoftwareAcademy);
+
+            var courseMock = new Mock<ICourse>();
+            courseMock.Setup(x => x.ToString()).Returns("");
+
+            //Act
+            var test = season.ListCourses();
+            //Assert
+            StringAssert.Contains("no course", test.ToString());
+
+        }
+    }
+}
