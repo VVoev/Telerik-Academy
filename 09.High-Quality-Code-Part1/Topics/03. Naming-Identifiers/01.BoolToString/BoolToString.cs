6fcb0b105770f4fdad01a424aa10f263850492fe
VVoev <vladovoev@yahoo.co.uk>
2017-03-11 14:03:26 +0200

    task 1 finished

---
+++ 09.High-Quality-Code-Part1/Topics/03. Naming-Identifiers/01.BoolToString/BoolToString.cs
@@ -0,0 +1,11 @@
+﻿namespace _01.BoolToString
+{
+    public class BoolToString
+    {
+        public static void Main(string[] args)
+        {
+            BoolToStringProgram stringValue = new BoolToStringProgram();
+            stringValue.ConvertBoolToString(false);
+        }
+    }
+}
---
+++ 09.High-Quality-Code-Part1/Topics/03. Naming-Identifiers/01.BoolToString/BoolToStringProgram.cs
@@ -0,0 +1,15 @@
+﻿namespace _01.BoolToString
+{
+    using System;
+    
+    public class BoolToStringProgram
+    {
+        public const int MaxCount = 6;
+
+        public void ConvertBoolToString(bool value)
+        {
+            string boolAsString = value.ToString();
+            Console.WriteLine(boolAsString);
+        }
+    }
+}
