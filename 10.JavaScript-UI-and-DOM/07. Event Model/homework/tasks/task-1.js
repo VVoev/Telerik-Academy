4a3e753750a451e4d5bc4e92dcc2f04ac3c3d7ff
VVoev <vladovoev@yahoo.co.uk>
2017-03-13 12:19:28 +0200

    finish homework events

--- 10.JavaScript-UI-and-DOM/07. Event Model/homework/tasks/task-1.js
+++ 10.JavaScript-UI-and-DOM/07. Event Model/homework/tasks/task-1.js
@@ -2,50 +2,121 @@
 
 /* 
 
-Create a function that takes an id or DOM element and:
-  
+ Create a function that takes an id or DOM element and:
 
-*/
+
+ */
 
 function solve(){
   return function (selector) {
 
-    if(typeof selector !== 'string' && selector.nodeType!==1){
-        throw {
-          name:"Invalid Argument",
-          message:"Invalid Argument Error"
+    function checkIfSelectorIsValid(selector) {
+      if(typeof  selector === 'string'){
+        selector = document.getElementById(selector);
+        if(selector === null){
+         throw 'Invalid'
         }
+        return selector;
+      }
+      else{
+        throw 'Invalid'
+      }
     }
 
-    function getDomElement(selector) {
-      var element;
-      if (selector.nodeType === 1 && selector.id) {
-        element = selector;
-        if (!document.body.contains(element)) {
-          throw {
-            name: 'InvalidDomElement',
-            message: 'InvalidDomElement'
-          }
-        }
+    function findContentSiblingIfAny(target){
+      var currentSibling = target.nextElementSibling;
+
+      if(currentSibling === null || currentSibling === undefined || currentSibling.className === buttonsC){
+        return null;
       }
-      else {
-        element = document.getElementById(selector);
-        if (element == undefined) {
-          throw {
-            name: 'InvalidDomElement',
-            message: 'InvalidDomElement'
-          }
-        }
+      else if (currentSibling.classname === contentC){
+        return currentSibling;
       }
 
-      return element;
+      findContentSiblingIfAny(currentSibling);
     }
 
-    
+    function changeState(event) {
+      var targetButton = event.target;
+      var targetButtonSibling = findContentSiblingIfAny(targetButton);
 
+      if(targetButtonSibling !== null){
+        if(targetButton.style.display ==''){
+          targetButton.style.display = 'none';
+          targetButton.innerHTML = 'show';
+        }
+        else{
+          targetButtonSibling.style.display ='';
+          targetButtonSibling.innerHTML = 'hide';
+        }
 
+      }
+    }
 
+    const buttonsC = 'button';
+    const contentC = 'content';
+
+    selector =  checkIfSelectorIsValid(selector);
+    var buttonClass = selector.getElementsByClassName(buttonsC);
+    var contentClass = selector.getElementsByClassName(contentC);
+
+    var buttons = [].slice.apply(buttonClass);
+    buttons.forEach(b=>{
+      b.textContent = "hide";
+      b.addEventListener('click',changeState);
+    })
   };
 };
 
 //module.exports = solve;
+
+// function solve() {
+//  return function(selector) {
+//  if (typeof selector === 'string') {
+//  selector = document.getElementById(selector);
+//  if (selector == null) {
+//  throw Error('selector not found');
+//  }
+//  } else if (!(selector instanceof HTMLElement)) {
+//  throw Error('invalid selector');
+//  }
+//
+//  var buttons = document.getElementsByClassName('button');
+//  var contents = document.getElementsByClassName('content');
+//
+//  for (var i = 0; i < buttons.length; i += 1) {
+//  buttons[i].textContent = 'hide';
+//  buttons[i].addEventListener('click', changeVisibility);
+//  }
+//  };
+//
+//  function changeVisibility(event) {
+//  var targetButton = event.target;
+//
+//  var targetButtonSibling = findSibling(targetButton);
+//  //if we found the correct sibling
+//  if (targetButtonSibling != null) {
+//  if (targetButtonSibling.style.display == '') {
+//  targetButtonSibling.style.display = 'none';
+//  targetButton.innerHTML = 'show';
+//  } else {
+//  targetButtonSibling.style.display = '';
+//  targetButton.innerHTML = 'hide';
+//  }
+//  }
+//  }
+//  function findSibling(targetEl) {
+//
+//  var currentSibling = targetEl.nextElementSibling;
+//
+//  if (currentSibling == null ||
+//  currentSibling == undefined ||
+//  currentSibling.className == 'button') {
+//  return null;
+//  } else if (currentSibling.className == 'content') {
+//  return currentSibling;
+//  }
+//
+//  finSibling(currentSibling);
+//  }
+//  }
