6157805465ef9f32db0436cae126246ca205799c
VVoev <vladovoev@yahoo.co.uk>
2017-03-17 17:11:04 +0200

    3 loop is now refactored

---
+++ 09.High-Quality-Code-Part1/Topics/06. Control-Flow-Conditional-Statements-and-Loops/homework/Homework/3.RefactorLoop/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _3.RefactorLoop
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int i = 0;
+            int divisior = 10;
+            int expectedValue = 40;
+            bool isValueFound = false;
+
+            for (i = 0; i < 100;)
+            {
+                if (i % divisior == 0)
+                {
+                    Console.WriteLine(i);
+                    if (i == expectedValue)
+                    {
+                        isValueFound = true;
+                    }
+
+                    i++;
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                    i++;
+                }
+            }
+            //// More code here
+            if (isValueFound)
+            {
+                Console.WriteLine("Value Found");
+            }
+        }
+    }
+}
