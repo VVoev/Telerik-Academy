c557b437215ef53fcc4579fe162eb14d8ae67ec8
VVoev <vladovoev@yahoo.co.uk>
2017-03-25 10:48:37 +0200

    task solved

--- 10.JavaScript-UI-and-DOM/Practical Exams/6-August-2015/task-3/Variant 1/Skeleton/tasks/task-3.js
+++ 10.JavaScript-UI-and-DOM/Practical Exams/6-August-2015/task-3/Variant 1/Skeleton/tasks/task-3.js
@@ -1,29 +1,30 @@
 function solve() {
     return function (selector) {
-        var template = [
-        '<h2 class="header">'+
-            'Appointments for <span class="month">{{month}}</span> <span class="year">{{year}}</span>'+
-        '</h2>'+
-        '{{#days}}'+
-            '<div class="col-date">' +
-                '<div class="date">{{day}}</div>' +
-                    '<div class="events">' +
-                         '{{#events}}' +
-                            '{{#if title}}' +
-                                '<div class="event {{importance}}" title="{{comment}}">' +
-                                '<div class="title">{{title}}</div>' +
-                                '<span class="time">at: {{time}}</span>' +
-                                '</div>' +
-                            '{{else}}' +
-                                '<div class="event {{importance}}">' +
-                                '<div class="title">Free slot</div>' +
-                                '</div>' +
-                            '{{/if}}' +
-                         '{{/events}}' +
-                    '</div>' +
-                '</div>' +
-        '{{/days}}'
-        ];
+        var template = `
+            <h2 class="header">
+                Appointments for <span class="{{month}}">August</span> <span class="{{year}}">{{year}}</span>
+            </h2>
+            
+            {{#days}}
+            <div class="col-date">
+                <div class="date">{{day}}</div>
+                    <div class="events">
+                    {{#events}}
+                        {{#if title}}
+                            <div class="event {{importance}}" title="{{title}}">
+                                <div class="title">{{title}}</div>
+                                <span class="{{time}}">at: {{time}}</span>
+                            </div>                        
+                        {{else}}
+                        <div class="event none">
+                            <div class="title">Free slot</div>
+                        </div>
+                        {{/if}}                               
+                    {{/events}}
+                    </div>   
+            </div>    
+            {{/days}}           
+            `
         document.getElementById(selector).innerHTML = template;
     };
 }
