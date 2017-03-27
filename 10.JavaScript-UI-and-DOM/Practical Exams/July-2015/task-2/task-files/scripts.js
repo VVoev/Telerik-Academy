df7645b8b645b52e3f0918ac529a0310ff32af39
VVoev <vladovoev@yahoo.co.uk>
2017-03-27 20:13:51 +0300

    jquery solved

--- 10.JavaScript-UI-and-DOM/Practical Exams/July-2015/task-2/task-files/scripts.js
+++ 10.JavaScript-UI-and-DOM/Practical Exams/July-2015/task-2/task-files/scripts.js
@@ -1,3 +1,17 @@
 $.fn.tabs = function () {
-
+    let $tabController = $(this);
+    $tabController.addClass('tabs-container');
+    $tabController.find('.tab-item-content').hide();
+    
+    $tabController.on('click','.tab-item-title',function () {
+        let $clickedElement = $(this);
+        $tabController.find('.tab-item-content').hide();
+        clearClass($tabController);
+        $clickedElement.next().show();
+        $clickedElement.parent().addClass('current');
+    })
+    
+    function clearClass($tabController) {
+        $tabController.find('.tab-item').removeClass('current');
+    }
 };
