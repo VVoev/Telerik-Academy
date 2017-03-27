b6fed3a6309aa7493458bd66cbb9f60d66efa37e
VVoev <vladovoev@yahoo.co.uk>
2017-03-27 11:03:36 +0300

    solved

---
+++ 10.JavaScript-UI-and-DOM/Practical Exams/6-August-2015/task-3/Variant 1/Skeleton/tasks/task-5.js
@@ -0,0 +1,35 @@
+function solve() {
+    return function (selector) {
+        var template = `
+                <div class="events-calendar">
+                    <h2 class="header">
+                Appointments for <span class="month">{{month}}</span> <span class="year">{{year}}</span>
+                    </h2>
+                    
+                    {{#days}}
+                     <div class="col-date">
+                        <div class="date">{{day}}</div>
+                        <div class="events">
+                        {{#events}}
+                          {{#if title}}
+                        <div class="event {{importance}}" title="no comment">
+                            <div class="title">{{title}}</div>
+                            <span class="time">at: {{time}}</span>
+                        </div>
+                        {{else}}
+                        <div class="event none">
+                                <div class="title">Free slot</div>
+                        </div>
+                        {{/if}}
+                        {{/events}}
+                        </div>
+                     </div>
+                    {{/days}}
+                    
+                    
+                 </div>
+         
+            `
+        document.getElementById(selector).innerHTML = template;
+    };
+}
