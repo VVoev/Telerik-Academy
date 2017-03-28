55f4a2dfd02420bee2b5662e120c208cc6b1b2c2
VVoev <vladovoev@yahoo.co.uk>
2017-03-28 09:58:37 +0300

    jquery task

--- 10.JavaScript-UI-and-DOM/Practical Exams/17-June-2014/task-2/task-resources/solved.js
+++ 10.JavaScript-UI-and-DOM/Practical Exams/17-June-2014/task-2/task-resources/solved.js
@@ -1,19 +1,58 @@
 /* globals $ */
 $.fn.gallery = function (columnsPerRow) {
 
-    var columnsPerRow = columnsPerRow || 4;
-    let $this = $(this);
-    let $imageContainer = $this.find('.image-container');
+    columnsPerRow = columnsPerRow || 4;
+
+    let $gallery = $(this);
+    let $imageContainer = $gallery.find('.image-container');
+    let $galleryList = $gallery.children('.gallery-list');
+    let $selected = $gallery.find('.selected');
+
+    let $currentImage = $selected.find('#current-image');
+    let $nextImage = $selected.find('#next-image');
+    let $prevImage = $selected.find('#previous-image');
+
+    $gallery.addClass('gallery');
+    $selected.hide();
 
     $imageContainer.each(function (index, elem) {
         if (index % columnsPerRow === 0) {
             $(elem).addClass('clearfix');
         }
+    });
+    
+    $galleryList.on('click','img',function () {
+        let $this = $(this);
+        let imgSrc = $this.attr('src');
+        let index = $this.data('info');
+        $('</div>').addClass('disabled-background').appendTo($gallery);
+        $galleryList.addClass('blurred')
+        $currentImage.attr('src',imgSrc);
+        $selected.show();
+    })
+
+    $currentImage.on('click',function () {
+        $selected.hide();
+        $galleryList.removeClass('blurred')
     })
 
 
-    $this.addClass('gallery');
-    $this.find('.selected').hide();
 
 
+
+
+
+
+
+    function validateIfNumberInRange(elem) {
+        if(elem>$imageContainer.length){
+            elem=1;
+            return elem;
+        }
+        if(elem<1){
+            elem = $imageContainer.length;
+            return elem;
+        }
+        return elem;
+    }
 };
