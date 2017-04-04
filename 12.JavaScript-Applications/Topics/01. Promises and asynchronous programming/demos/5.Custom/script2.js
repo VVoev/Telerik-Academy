16e739f1302c825087687b534da7b9e2b0df5c76
VVoev <vladovoev@yahoo.co.uk>
2017-04-04 13:32:46 +0300

    custom

--- 12.JavaScript-Applications/Topics/01. Promises and asynchronous programming/demos/5.Custom/script2.js
+++ 12.JavaScript-Applications/Topics/01. Promises and asynchronous programming/demos/5.Custom/script2.js
@@ -1,3 +1,62 @@
-/**
- * Created by Vladimir on 4/4/2017.
- */
+let catalog = {};
+
+let div = document.createElement('div');
+div.id = 'Catalog';
+let ul = document.createElement('ul');
+div.innerHTML = "Catalog";
+
+let furnitures = ['Table', 'Chair', 'Lamp', 'PC', 'Door'];
+
+for (let i = 0; i < 5; i += 1) {
+    let li = document.createElement('li');
+    li.innerHTML = furnitures[i];
+    let dis = i % 2 === 0;
+    var x;
+    if (dis) {
+
+        x = ((x + 1) / 100) * 85 || 37;
+    }
+    else {
+        x = (i + 1) * 23 * 1.5;
+    }
+    catalog[i] = {
+        id: i,
+        names: furnitures[i],
+        discount: dis,
+        price: x
+    };
+    ul.appendChild(li);
+
+}
+
+function giveMeCheapest(items) {
+    let cheapestPrice = 9999;
+    let x = [];
+    for (var item in items) {
+        if(items[item].price<cheapestPrice){
+            cheapestPrice = items[item].price;
+            x.push(items[item]);
+        }
+    }
+    return x;
+}
+let promise = new Promise((resolve, reject)=> {
+    resolve(giveMeCheapest(catalog));
+    reject("Site is broken");
+})
+
+
+div.appendChild(ul);
+document.body.appendChild(div);
+
+
+promise
+    .then(function (data) {
+        let sale = document.getElementById('onsale');
+        sale.style.color='blue';
+        sale.innerHTML+=data[0].names.toLowerCase()+" "+data[0].price+" bgn";
+    })
+    .catch(function (error) {
+        console.log(error)
+    })
+
