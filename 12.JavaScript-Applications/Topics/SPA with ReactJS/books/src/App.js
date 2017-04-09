b50dbc1217ad7d62e756167776b4bfd51a722215
VVoev <vladovoev@yahoo.co.uk>
2017-04-09 16:34:59 +0300

    initial setup react project

---
+++ 12.JavaScript-Applications/Topics/SPA with ReactJS/books/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        Welcome to my App
+      </div>
+    );
+  }
+}
+
+
---
+++ 12.JavaScript-Applications/Topics/SPA with ReactJS/books/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+import $ from 'jquery'
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+);
