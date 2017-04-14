bf8d4bf1597713fc08a8e783eb5a0795e2ce0ebe
VVoev <vladovoev@yahoo.co.uk>
2017-04-14 15:13:09 +0300

    Assertion Demo added

---
+++ 11.High-Quality-Code-Part-2/01. Defensive-Programming-and-Exceptions/demos/AssertionDemo/AssertionDemo.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+
+namespace AssertionDemo
+{
+ public class AssertionDemo
+    {
+        public static void Main(string[] args)
+        {
+            Debug.Assert(PerformAction(),  "Could not perform action");
+            var studentsCalculator = new StudentGradesCalculator(new int[] { 1, 2, 3, 4, 5 });
+
+            var c = studentsCalculator.GetAverageStudentGrade();
+            //
+        }
+
+        private static bool PerformAction()
+        {
+            System.Console.WriteLine("Action Performed");
+            return true;
+        }
+    }
+}
