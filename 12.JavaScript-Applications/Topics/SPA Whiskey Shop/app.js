f370fd1583be2a1eb02143dbdb199749d384ac93
VVoev <vladovoev@yahoo.co.uk>
2017-04-15 19:54:46 +0300

    added 3 handlebars templates

--- 12.JavaScript-Applications/Topics/SPA Whiskey Shop/app.js
+++ 12.JavaScript-Applications/Topics/SPA Whiskey Shop/app.js
@@ -2,17 +2,24 @@ let sammyApp = Sammy('#root', function () {
     let content = $('#content');
 
     //this => sammy.application
-
-    this.get('#/quit', function () {
-        templates.get('quit')
+    this.get('#/Login', function () {
+        templates.get('login')
             .then(function (template) {
                 //template -> handlebars.compile(tempalteString)
                 content.html(template());
             });
     })
 
-    this.get('#/items', function () {
-        templates.get('items')
+    this.get('#/Register', function () {
+        templates.get('register')
+            .then(function (template) {
+                //template -> handlebars.compile(tempalteString)
+                content.html(template());
+            });
+    })
+
+    this.get('#/Home', function () {
+        templates.get('home')
             .then(function (template) {
                 //template -> handlebars.compile(tempalteString)
                 content.html(template());
