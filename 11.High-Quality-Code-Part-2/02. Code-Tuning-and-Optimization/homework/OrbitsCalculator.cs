d3cbc7b24c77e2b83c5ee313a3781c43d8a63f41
VVoev <vladovoev@yahoo.co.uk>
2017-04-18 15:05:41 +0300

    added homework Optimization

--- 11.High-Quality-Code-Part-2/02. Code-Tuning-and-Optimization/homework/OrbitsCalculator.cs
+++ 11.High-Quality-Code-Part-2/02. Code-Tuning-and-Optimization/homework/OrbitsCalculator.cs
@@ -93,10 +93,8 @@ namespace SolarSystem
 
         private void EarthRotation()
         {
-			for (decimal step = 0; step <= 360; step+=0.00005m)
-			{
-				EarthRotationAngle = ((double)step) * Days / EarthRotationPeriod;
-			}
+
+            EarthRotationAngle = EarthYear * Days / SunRotationPeriod;
             Update("EarthRotationAngle");
         }
 
