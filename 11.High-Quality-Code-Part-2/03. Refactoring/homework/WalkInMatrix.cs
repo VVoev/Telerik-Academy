ac3a42f2b61a755093df5331bd5593e3ca69c285
VVoev <vladovoev@yahoo.co.uk>
2017-04-27 11:58:53 +0300

    homework finished

---
+++ 11.High-Quality-Code-Part-2/03. Refactoring/homework/WalkInMatrix.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace MatrixWalking
+{
+    public static class WalkInMatrix
+    {
+        public static void Change(ref int dx, ref int dy)
+        {
+            int[] dirX = { 1, 1, 1, 0, -1, -1, -1, 0 };
+
+
+            int[] dirY = { 1, 0, -1, -1, -1, 0, 1, 1 };
+            int cd = 0;
+            for (int count = 0; count < 8; count++)
+            {
+                if (dirX[count] == dx && dirY[count] == dy)
+                {
+                    cd = count;
+                    break;
+                }
+            }
+            if (cd == 7)
+            {
+                dx = dirX[0]; dy = dirY[0]; return;
+            }
+            dx = dirX[cd + 1];
+
+
+            dy = dirY[cd + 1];
+        }
+        public static bool Proverka(int[,] arr, int x, int y)
+        {
+            int[] dirX = { 1, 1, 1, 0, -1, -1, -1, 0 };
+            int[] dirY = { 1, 0, -1, -1, -1, 0, 1, 1 };
+            for (int i = 0; i < 8; i++)
+            {
+                if (x + dirX[i] >= arr.GetLength(0) || x + dirX[i] < 0)
+                {
+                    dirX[i] = 0;
+                }
+
+
+                if (y + dirY[i] >= arr.GetLength(0) || y + dirY[i] < 0)
+                {
+                    dirY[i] = 0;
+                }
+            }
+            for (int i = 0; i < 8; i++)
+            {
+                if (arr[x + dirX[i], y + dirY[i]] == 0)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public static void FindCell(int[,] arr, out int x, out int y)
+        {
+            x = 0;
+            y = 0;
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(0); j++)
+                {
+                    if (arr[i, j] == 0)
+                    {
+                        x = i;
+                        y = j;
+                        return;
+                    }
+                }
+            }
+        }
+
+
+    }
+}
