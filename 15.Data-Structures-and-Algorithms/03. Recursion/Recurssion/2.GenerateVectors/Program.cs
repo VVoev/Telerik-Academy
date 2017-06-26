36ace579e684e19c818cf4d197b96fd6ce4aa137
VVoev <vladovoev@yahoo.co.uk>
2017-06-27 00:42:33 +0300

    done with demos

---
+++ 15.Data-Structures-and-Algorithms/03. Recursion/Recurssion/2.GenerateVectors/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Vectors01Generator
+{
+    static void Main()
+    {
+        int number = 5;
+
+        int[] vector = new int[number];
+
+        Generate01(number - 1, vector);
+    }
+
+    private static void Generate01(int index, int[] vector)
+    {
+        if(index == -1)
+        {
+            Print(vector);
+        }
+
+        else
+        {
+            for (int i = 0; i < 2; i++)
+            {
+                vector[index] = i;
+                Generate01(index - 1, vector);
+            }
+        }
+    }
+
+    static void Print(int[] vector)
+    {
+        foreach (int v in vector)
+        {
+            Console.Write($"{v} ");
+        }
+        Console.WriteLine();
+    }
+}
---
+++ 15.Data-Structures-and-Algorithms/03. Recursion/Recurssion/7.Fibonacci Fast/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _7.Fibonacci_Fast
+{
+    class Program
+    {
+        const int size = 1000;
+        static decimal[] fib = new decimal[size];
+        static decimal recursiveCallCounter = 0;
+
+
+
+
+        static void Main(string[] args)
+        {
+            int number = 100;
+            decimal fib = Fibonachi(number);
+            System.Console.WriteLine($" Fibonachi number {number} : {fib}");
+            System.Console.WriteLine($"Recursive Calls : {recursiveCallCounter}");
+        }
+
+        private static decimal Fibonachi(int number)
+        {
+            recursiveCallCounter++;
+            if(fib[number] == 0)
+            {
+                
+                if(number ==1 || number == 2)
+                {
+                    fib[number] = 1;
+                }
+                else
+                {
+                    fib[number] = Fibonachi(number - 1) + Fibonachi(number - 2);
+                }
+            }
+
+            return fib[number];
+        }
+    }
+}
