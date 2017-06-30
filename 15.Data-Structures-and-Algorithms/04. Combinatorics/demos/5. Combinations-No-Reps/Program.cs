a73e77b3bc3efc1badb9a9a588efec3f8667f691
VVoev <vladovoev@yahoo.co.uk>
2017-06-30 13:05:03 +0300

    recursion and combinatorics

---
+++ 15.Data-Structures-and-Algorithms/04. Combinatorics/demos/5. Combinations-No-Reps/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace _5.Combinations_No_Reps
+{
+    class Program
+    {
+        const int n = 5;
+        const int k = 3;
+
+        static string[] objects = new string[n] { "banana", "apple", "orange", "strawberry", "raspberry" };
+        static int[] arr = new int[k];
+
+        static void Main()
+        {
+            GenerateAllCombinations(0,0);
+        }
+
+        private static void GenerateAllCombinations(int index,int start)
+        {
+            if(index >=k)
+            {
+                Print(arr);
+            }
+
+            else
+            {
+                for (int i = start; i < n; i++)
+                {
+                    arr[index] = i;
+                    GenerateAllCombinations(index + 1,i+1);
+                }
+            }
+        }
+
+        private static void Print(int[] arr)
+        {
+            Console.Write(string.Join(" ", arr) + "=>");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write((objects[arr[i]] + " "));
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ 15.Data-Structures-and-Algorithms/04. Combinatorics/demos/6. Combinations+Repetitions/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6.Combinations_Repetitions
+{
+    class Program
+    {
+        const int n = 5;
+        const int k = 3;
+
+        static string[] objects = new string[n] { "banana", "apple", "orange", "strawberry", "raspberry" };
+        static int[] arr = new int[k];
+
+        static void Main()
+        {
+            GenerateAllCombinations(0);
+        }
+
+        private static void GenerateAllCombinations(int index)
+        {
+            if (index >= arr.Length)
+            {
+                Print(arr);
+            }
+
+            else
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    arr[index] = i;
+                    GenerateAllCombinations(index + 1);
+                }
+            }
+        }
+
+        private static void Print(int[] arr)
+        {
+            Console.Write(string.Join(" ", arr) + "=>");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write((objects[arr[i]] + " "));
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
