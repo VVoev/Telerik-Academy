4698a66183b0878bf93530c9a9b6f3a04ad4bcc4
VVoev <vladovoev@yahoo.co.uk>
2017-07-14 22:22:38 +0300

    some exams

---
+++ 15.Data-Structures-and-Algorithms/some/Solution1/GrafBasics/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrafBasics
+{
+    class Program
+    {
+        static string input = @"7
+8
+1 4
+1 7
+2 6
+2 7
+3 5
+3 6
+3 7
+5 6";
+
+
+        static void Main(string[] args)
+        {
+            FakeInput();
+            //PrintGrapfWithMatrix();
+            PrintGrafWithAdjustencyList();
+        }
+
+        private static void PrintGrafWithAdjustencyList()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var m = int.Parse(Console.ReadLine());
+
+            var vertices = new LinkedList<int>[n+1];
+            for (int i = 0; i < n; i++)
+            {
+                var edge = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+                var from = edge[0];
+                var to = edge[1];
+
+                vertices[from].AddLast(to);
+
+            }
+        }
+
+        private static void PrintGrapfWithMatrix()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var m = int.Parse(Console.ReadLine());
+
+            bool[,] matrix = new bool[n + 1, m + 1];
+            for (int i = 0; i < n; i++)
+            {
+                var edge = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+                var from = edge[0];
+                var to = edge[1];
+                matrix[from, to] = true;
+                matrix[to, from] = true;
+            }
+
+            Print(matrix);
+        }
+
+        static void FakeInput()
+        {
+            Console.SetIn(new StringReader(input));
+        }
+        private static void Print(bool[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write(matrix[i, j] ? 1 : 0);
+                    Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ 15.Data-Structures-and-Algorithms/some/Solution1/sasa/Program.cs
@@ -0,0 +1,76 @@
+﻿namespace Sasa
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var adjList = new List<int>[n];
+            for (int i = 0; i < n; i++)
+            {
+                var line = Console.ReadLine();
+                if (line == "None")
+                {
+                    adjList[i] = new List<int>();
+                }
+                else
+                {
+                    adjList[i] = line.Split(' ').Select(int.Parse).ToList();
+                }
+
+            }
+
+            while (true)
+            {
+                var line = Console.ReadLine();
+                if (line == "Have a break")
+                {
+                    break;
+                }
+
+                var strs = line.Split(' ');
+                var a = int.Parse(strs[0]);
+                var b = int.Parse(strs[1]);
+
+                if (adjList[a].Contains(b))
+                {
+                    Console.WriteLine("There is a direct flight.");
+                }
+
+                else
+                {
+                    var visited = new bool[n];
+                    DFS(a, adjList, visited);
+
+                    if (visited[b])
+                    {
+                        Console.WriteLine("There are flights, unfortunately they are not direct, grandma :(");
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("No flights available.");
+                    }
+                }
+            }
+        }
+
+        static void DFS(int x, List<int>[] adjList, bool[] isVisited)
+        {
+            isVisited[x] = true;
+            foreach (var y in adjList[x])
+            {
+                if (!isVisited[y])
+                {
+                    DFS(y, adjList, isVisited);
+                }
+            }
+        }
+    }
+}
