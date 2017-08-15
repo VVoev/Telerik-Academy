a18a8d361fe089ecd3c6bb10b44e2b68ee95441f
VVoev <vladovoev@yahoo.co.uk>
2017-08-15 13:05:59 +0300

    resursive task

---
+++ 15.Data-Structures-and-Algorithms/Exam-Preparation/ExamPreparation/[Recurssion]MessageInABottle/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _Recurssion_MessageInABottle
+{
+    class Program
+    {
+        public static List<KeyValuePair<char, string>> ciphers = new List<KeyValuePair<char, string>>();
+        public static string message;
+        public static int count;
+        public static List<string> solutions = new List<string>();
+        static void Main(string[] args)
+        {
+            /* Input
+            1122
+            A1B12C11D2
+            */
+            message = Console.ReadLine();
+            string cypher = Console.ReadLine();
+
+            char key = char.MinValue;
+            var value = new StringBuilder();
+
+            for (int i = 0; i < cypher.Length; i++)
+            {
+                //if (char.IsLetter(cypher[i]))
+                if (cypher[i] >= 'A' && cypher[i] <= 'Z')
+                {
+                    if(key != char.MinValue)
+                    {
+                        ciphers.Add(new KeyValuePair<char, string>(key, value.ToString()));
+                        value.Clear();
+                    }
+                    key = cypher[i];
+                }
+                else
+                {
+                    value.Append(cypher[i]);
+                }
+            }
+
+            if (key != char.MinValue)
+            {
+                ciphers.Add(new KeyValuePair<char, string>(key, value.ToString()));
+                value.Clear();
+            }
+
+            Solve(0,new StringBuilder());
+            Console.WriteLine(count);
+            solutions.OrderBy((x=>x.Length));
+            PrintEachSolution(solutions);
+        }
+
+        private static void PrintEachSolution(List<string> solutions)
+        {
+            foreach (var sol in solutions)
+            {
+                Console.WriteLine(sol);
+            }
+        }
+
+        static void Solve(int secretMessageLenght, StringBuilder sb)
+        {
+            if(secretMessageLenght == message.Length)
+            {
+                count++;
+                solutions.Add(sb.ToString());
+                return;
+            }
+
+            foreach (var cipher in ciphers)
+            {
+                if (message.Substring(secretMessageLenght).StartsWith(cipher.Value))
+                {
+                    sb.Append(cipher.Key);
+                    Solve(secretMessageLenght + cipher.Value.Length,sb);
+                    sb.Length--;
+                }
+            }
+        }
+    }
+}
---
+++ 15.Data-Structures-and-Algorithms/Exam-Preparation/ExamPreparation/[Recurssion]MessageInABottle/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("[Recurssion]MessageInABottle")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("[Recurssion]MessageInABottle")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f8de97f4-5a1a-45e7-b5ed-ee45f7b18c65")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
