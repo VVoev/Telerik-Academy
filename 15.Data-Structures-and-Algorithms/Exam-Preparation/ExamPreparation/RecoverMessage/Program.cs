90e0f677257838235b8f45ff3fdc7467077715cf
VVoev <vladovoev@yahoo.co.uk>
2017-08-22 15:08:11 +0300

    exam preparation

---
+++ 15.Data-Structures-and-Algorithms/Exam-Preparation/ExamPreparation/RecoverMessage/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace RecoverMessage
+{
+    class Program
+    {
+        static SortedDictionary<char,Node> graph = new SortedDictionary<char, Node>();
+
+        static Node GetNodeByChar(char symbol)
+        {
+            if (graph.ContainsKey(symbol))
+            {
+                return graph[symbol];
+            }
+
+            var newNode = new Node() { Value = symbol };
+            graph.Add(symbol, newNode);
+            return newNode;
+        }
+
+        static void Main(string[] args)
+        {
+            int messageNumber = int.Parse(Console.ReadLine());
+            var noIncomingEdges = new SortedSet<char>();
+
+            for (int i = 0; i < messageNumber; i++)
+            {
+                string currentMessage = Console.ReadLine();
+
+                for (int j = 1; j <= currentMessage.Length; j++)
+                {
+                    Node previousNode = GetNodeByChar(currentMessage[j-1]);
+                    Node currentNode = GetNodeByChar(currentMessage[j]);
+                    previousNode.Successors.Add(currentNode);
+                    currentNode.Parrents.Add(previousNode);
+
+                    previousNode = currentNode;
+                }
+            }
+
+            foreach (var node in graph.Values)
+            {
+                if (node.Parrents.Count == 0)
+                {
+                    noIncomingEdges.Add(node.Value);
+                }
+            }
+            Console.WriteLine();
+
+        }
+    }
+
+    class Node
+    {
+        public Node()
+        {
+            this.Successors = new HashSet<Node>();
+            this.Parrents = new HashSet<Node>();
+        }
+        public char Value { get; set; }
+
+        public ICollection<Node> Successors { get; set; }
+
+        public ICollection<Node> Parrents { get; set; }
+    }
+}
---
+++ 15.Data-Structures-and-Algorithms/Exam-Preparation/ExamPreparation/[BFS]Labyrinth/Program.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _3DLabyrinth
+{
+    class Labyrinth
+    {
+        static char[,,] cube;
+        static long[,,] visited;
+
+        //check zero
+
+        static void BFS(Coordinates start)
+        {
+            long count = 0;
+            Queue<Coordinates> queue = new Queue<Coordinates>();
+            queue.Enqueue(start);
+            visited[start.Level, start.Row, start.Col] = count;
+
+            while (queue.Count != 0)
+            {
+                var coords = queue.Dequeue();
+                count = visited[coords.Level, coords.Row, coords.Col];
+
+                if (cube[coords.Level, coords.Row, coords.Col] == 'U')
+                {
+                    if (coords.Level + 1 >= cube.GetLength(0))
+                    {
+                        Console.WriteLine(count + 1);
+                        break;
+                    }
+
+                    if (visited[coords.Level + 1, coords.Row, coords.Col] == 0 && cube[coords.Level + 1, coords.Row, coords.Col] != '#')
+                    {
+                        Coordinates next = new Coordinates(coords.Level + 1, coords.Row, coords.Col);
+                        visited[next.Level, next.Row, next.Col] = count + 1;
+                        queue.Enqueue(next);
+                    }
+                }
+
+                if (cube[coords.Level, coords.Row, coords.Col] == 'D')
+                {
+                    if (coords.Level - 1 < 0)
+                    {
+                        Console.WriteLine(count + 1);
+                        break;
+                    }
+
+                    if (visited[coords.Level - 1, coords.Row, coords.Col] == 0 && cube[coords.Level - 1, coords.Row, coords.Col] != '#')
+                    {
+                        Coordinates next = new Coordinates(coords.Level - 1, coords.Row, coords.Col);
+                        visited[next.Level, next.Row, next.Col] = count + 1;
+                        queue.Enqueue(next);
+                    }
+                }
+
+                if (coords.Row - 1 >= 0 && cube[coords.Level, coords.Row - 1, coords.Col] != '#')
+                {
+                    if (visited[coords.Level, coords.Row - 1, coords.Col] == 0)
+                    {
+                        Coordinates next = new Coordinates(coords.Level, coords.Row - 1, coords.Col);
+                        visited[next.Level, next.Row, next.Col] = count + 1;
+                        queue.Enqueue(next);
+                    }
+                }
+
+                if (coords.Row + 1 < cube.GetLength(1) && cube[coords.Level, coords.Row + 1, coords.Col] != '#')
+                {
+                    if (visited[coords.Level, coords.Row + 1, coords.Col] == 0)
+                    {
+                        Coordinates next = new Coordinates(coords.Level, coords.Row + 1, coords.Col);
+                        visited[next.Level, next.Row, next.Col] = count + 1;
+                        queue.Enqueue(next);
+                    }
+                }
+
+                if (coords.Col - 1 >= 0 && cube[coords.Level, coords.Row, coords.Col - 1] != '#')
+                {
+                    if (visited[coords.Level, coords.Row, coords.Col - 1] == 0)
+                    {
+                        Coordinates next = new Coordinates(coords.Level, coords.Row, coords.Col - 1);
+                        visited[next.Level, next.Row, next.Col] = count + 1;
+                        queue.Enqueue(next);
+                    }
+                }
+
+                if (coords.Col + 1 < cube.GetLength(2) && cube[coords.Level, coords.Row, coords.Col + 1] != '#')
+                {
+                    if (visited[coords.Level, coords.Row, coords.Col + 1] == 0)
+                    {
+                        Coordinates next = new Coordinates(coords.Level, coords.Row, coords.Col + 1);
+                        visited[next.Level, next.Row, next.Col] = count + 1;
+                        queue.Enqueue(next);
+                    }
+                }
+            }
+        }
+
+        static void Main()
+        {
+            string[] startDimensions = Console.ReadLine().Split(' ');
+
+            int startLevel = int.Parse(startDimensions[0]);
+            int startRow = int.Parse(startDimensions[1]);
+            int startCol = int.Parse(startDimensions[2]);
+
+            string[] dimensions = Console.ReadLine().Split(' ');
+
+            int levels = int.Parse(dimensions[0]);
+            int rows = int.Parse(dimensions[1]);
+            int cols = int.Parse(dimensions[2]);
+
+            cube = new char[levels, rows, cols];
+            visited = new long[levels, rows, cols];
+
+            for (int i = 0; i < levels; i++)
+            {
+                for (int j = 0; j < rows; j++)
+                {
+                    //can be optimized
+                    string currentRow = Console.ReadLine();
+
+                    for (int k = 0; k < currentRow.Length; k++)
+                    {
+                        cube[i, j, k] = currentRow[k];
+                    }
+                }
+            }
+
+            Coordinates start = new Coordinates(startLevel, startRow, startCol);
+
+            BFS(start);
+        }
+    }
+
+    public class Coordinates
+    {
+        public int Level { get; private set; }
+        public int Row { get; private set; }
+        public int Col { get; private set; }
+
+        public Coordinates(int level, int row, int col)
+        {
+            this.Level = level;
+            this.Row = row;
+            this.Col = col;
+        }
+    }
+}
