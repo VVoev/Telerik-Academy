459b0e06938817839ac17269bf9bf3d47fd29862
VVoev <vladovoev@yahoo.co.uk>
2017-08-26 00:56:07 +0300

    Doge taks

---
+++ 15.Data-Structures-and-Algorithms/Exam-Preparation/ExamPreparation/[DP]DogeCoin/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _DP_DogeCoin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cointValue = 1;
+            var input = Console.ReadLine().Split(' ').ToArray();
+            var rows = int.Parse(input[0]);
+            var cols = int.Parse(input[1]);
+            var matrix = new int[rows, cols];
+
+            var numberOfCoins = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numberOfCoins; i++)
+            {
+                var cordinates = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+                var rowCord = cordinates[0];
+                var colCord = cordinates[1];
+                    matrix[rowCord, colCord]++;
+            }
+            var dp = new int[rows, cols];
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    int maxAbove = 0;
+                    int MaxLeft = 0;
+
+                    if (row - 1 >= 0)
+                    {
+                        maxAbove = dp[row-1, col];
+                    }
+                    if (col - 1 >= 0)
+                    {
+                        MaxLeft = dp[row, col - 1];
+                    }
+
+                    dp[row, col] = Math.Max(maxAbove, MaxLeft) + matrix[row, col];
+                }
+            }
+
+            Console.WriteLine(dp[rows-1,cols-1]);
+
+
+           
+
+        }
+    }
+}
---
+++ 15.Data-Structures-and-Algorithms/Exam-Preparation/ExamPreparation/[DP]HelpDoge/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Numerics;
+using System.Linq;
+class HelpDoge
+{
+    const int Dogevalue = -1;
+    public static int fx, fy;
+    static void Main()
+    {
+
+        var field = Input();
+
+        for (int x = 0; x < field.GetLength(0); x++)
+        {
+            for (int y = 0; y < field.GetLength(1); y++)
+            {
+                if (x == 0 && y == 0)
+                {
+                    field[0, 0] = 1;
+                    continue;
+                }
+                if (field[x, y] == Dogevalue)
+                {
+                    field[x, y] = 0;
+                    continue;
+                }
+                if (x == 0)
+                {
+                    field[0, y] = field[0, y - 1];
+                    continue;
+                }
+                if (y == 0)
+                {
+                    field[x, 0] = field[x - 1, 0];
+                    continue;
+                }
+                field[x, y] = field[x - 1, y] +
+                              field[x, y - 1];
+            }
+
+        }
+        Console.WriteLine("{0}", field[fx, fy]);
+
+
+    }
+
+    private static BigInteger[,] Input()
+    {
+        //dimensions of the matrix
+        var line = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        int n = line[0];
+        int m = line[1];
+
+        //cordinate for exiting the labirinth
+        line = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        fx = line[0];
+        fy = line[1];
+
+        BigInteger[,] field = new BigInteger[n, m];
+
+        // enemies
+        int enemies = int.Parse(Console.ReadLine());
+        for (int i = 0; i < enemies; i++)
+        {
+            line = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int x = line[0];
+            int y = line[1];
+            field[x, y] = Dogevalue;
+        }
+        return field;
+    }
+}
