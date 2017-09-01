3bf08bcf21c1e06d332d16cd41ac6dd1164778e7
VVoev <vladovoev@yahoo.co.uk>
2017-09-01 08:13:23 +0300

    renewal

---
+++ 15.Data-Structures-and-Algorithms/Exam-Preparation/ExamPreparation/[Graph]Renewal/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _Graph_Renewal
+{
+    class Program
+    {
+
+        static List<string> roads = new List<string>();
+        static List<string> builds = new List<string>();
+        static List<string> destroy = new List<string>();
+        static void Main(string[] args)
+        {
+            var N = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < N; i++)
+            {
+                roads.Add(Console.ReadLine());
+            }
+
+            for (int i = 0; i < N; i++)
+            {
+                builds.Add(Console.ReadLine());
+            }
+
+            for (int i = 0; i < N; i++)
+            {
+                destroy.Add(Console.ReadLine());
+            }
+
+        }
+    }
+}
